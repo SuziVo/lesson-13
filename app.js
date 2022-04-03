8ecc6574bb815622a8555adedc503b5edad31d84
suzi <suzivoskanian@gmail.com>
2022-04-03 22:12:58 +0400

    create js

---
+++ app.js
@@ -0,0 +1,34 @@
+let userName = "suzi"
+let userAge = 20;
+let isGirl = true;
+let myUndefined;
+let myNull = null;
+
+console.log(typeof userAge, typeof userName, typeof isGirl, typeof myUndefined, typeof myNull)
+
+
+let myName = "suzi"
+let myHobby = "drawing"
+let yearOfBirth = 1998;
+let currentYear=2022;
+
+console.log(typeof myName, typeof myHobby, typeof yearOfBirth, typeof currentYear)
+
+// console.log("myName", typeof myName);
+// console.log("myHobby", typeof myHobby);
+// console.log("yearOfBirth", typeof yearOfBirth);
+// console.log("currentYear", typeof currentYear);
+
+let substraction= 2022 - 1998;
+
+
+console.log("My name is " + myName);
+console.log("I am "  + substraction + " years old");
+console.log("My Hobby is " + myHobby);
+
+let myTextExample = `My name is ${userName}` ;
+let myTextEcample= `I am ${substraction} years old `;
+let myTextExampleHobby=`My hobby is ${myHobby}`;
+console.log(myTextExample);
+console.log(myTextEcample);
+console.log(myTextExampleHobby);
